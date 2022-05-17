08fb25e9200552725c7c5836915d60f1bf49a93e
Andres Roman <afromana@unal.edu.co>
2022-05-17 09:42:56 -0500

    Basic structure

---
+++ src/pages/ForgotPassword.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {useState} from 'react'
+
+
+function ForgotPassword() {
+  return (
+    <div>
+      Forgot Password
+    </div>
+  )
+}
+
+export default ForgotPassword
